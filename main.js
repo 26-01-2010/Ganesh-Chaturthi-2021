84d8e73639fc46df38f1a19754e4b9573ef8bf73
26-01-2010 <71710593+26-01-2010@users.noreply.github.com>
2021-09-11 20:18:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function pictureGanpati()
+{
+    window.location = "Page 2.html";
+}
+function sthapanaVideo()
+{
+    window.location = "sthapana_video.html";
+}
+function eveningPooja()
+{
+    window.location = "eveningPooja.html";
+}
+function morningPoojaDay2()
+{
+    window.location = "morningPoojaDay2.html";
+}
+function visarjanPoojaDay2()
+{
+    window.location = "visarjan pooja.html";
+}
+function videos()
+{
+    window.location = "videos.html";
+}
